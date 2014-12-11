8d145c642795676ec2e1b39a35739a9e81dc95a3
Manish Gupta <123manis@gmail.com>
2014-12-11 14:58:56 +0530

    Update FileCache.java

--- AlbumSample/src/main/src/com/nucleartip/album/data/FileCache.java
+++ AlbumSample/src/main/src/com/nucleartip/album/data/FileCache.java
@@ -2,7 +2,7 @@
  * Utility class for storing downloaded images into two different cache,
  * this class is used along with a list view for better processing of image data
  */
-package com.nucleartip.album.data;
+package com.eaxmple.memory;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -15,16 +15,20 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
+import android.net.Uri;
+import android.provider.MediaStore;
 
 public class FileCache {
 
 	private static File cacheDir;
-	private static final int MAX_SIZE = 100;
+	private static final int MAX_SIZE = 500;
 	private int currentCount = 0;
 	private static FileCache fCache;
 	private MemoryCache mChache;
-	
+	private Context mContext;
+
 	private FileCache(Context context){
+		mContext = context;
 		cacheDir = context.getCacheDir();
 		File[] files = cacheDir.listFiles();
 		mChache = MemoryCache.getInstance();
@@ -33,6 +37,9 @@ public class FileCache {
 		}else{
 			currentCount = 0;
 		}
+	}
+	private void refreshContxt(Context context){
+		this.mContext = context;
 	}
 	/**
 	 * Returns a non null instance of file cache
@@ -44,6 +51,7 @@ public class FileCache {
 			fCache = new FileCache(context);
 			return fCache;
 		}
+		fCache.refreshContxt(context);
 		return fCache;
 	}
 	/**
@@ -51,7 +59,7 @@ public class FileCache {
 	 * @param uri
 	 * @return
 	 */
-	public Bitmap getBitmapFromCache(String uri){
+	public Bitmap getBitmapFromCache(String uri,int width,int height){
 		Bitmap map = null;
 		try{
 			if(mChache.getBitmapFromMemCache(uri)!=null){
@@ -62,9 +70,11 @@ public class FileCache {
 			if(f.exists()){
 				Options opts = new BitmapFactory.Options();
 				opts.inSampleSize = 1;
+				opts.outHeight=height;
+				opts.outWidth=width;
 				opts.inPurgeable = true;
 				return BitmapFactory.decodeFile(f.getAbsolutePath(), opts);
-				
+
 			}
 		}catch(Exception e){
 			e.printStackTrace();
@@ -82,7 +92,7 @@ public class FileCache {
 	 */
 	public Bitmap getFile(Context context,String uri,int width,int height){
 		Bitmap map = null;
-		map = getBitmapFromCache(uri);
+		map = getBitmapFromCache(uri,width,height);
 		if(map != null){
 			return map;
 		}else{
@@ -90,6 +100,8 @@ public class FileCache {
 			File f = new File(cacheDir,fileName);
 			InputStream is = null;
 			FileOutputStream fos = null;
+			Bitmap mMap = null;
+
 			try{
 				if(currentCount > MAX_SIZE){
 					if(cacheDir.listFiles() !=null){
@@ -97,36 +109,71 @@ public class FileCache {
 					}
 					currentCount--;
 				}
-				URL aUrl = new URL(uri);
-				URLConnection conn = aUrl.openConnection();
-				conn.setUseCaches(true);
-				conn.connect();
-				is = conn.getInputStream();
-				fos = new FileOutputStream(f);
-				byte[] data = new byte[1024];
-				int n = 0;
-				while((n=is.read(data))!=-1){
-					fos.write(data, 0, n);
+
+				if(uri.startsWith(MediaStore.Images.Media.
+						EXTERNAL_CONTENT_URI.toString())){
+					// First lets fetch thumbail directly
+					Uri temp = Uri.parse(uri);
+					long id = Long.parseLong(temp.getLastPathSegment());
+					mMap = MediaStore.Images.Thumbnails.
+							getThumbnail(mContext.getContentResolver(),id,
+									MediaStore.Images.Thumbnails.MINI_KIND,null);
+					if(mMap == null){
+						BitmapFactory.Options options = new BitmapFactory.Options();
+						options.inJustDecodeBounds = true;
+						BitmapFactory.decodeFile(uri, options);
+						options.inSampleSize = calculateInSampleSize(options, width, height);
+						options.inJustDecodeBounds = false;
+
+						mMap = BitmapFactory.decodeFile(uri, options);
+
+					}
+
+
+				}else if(uri.contains("http")){
+					// Internet
+					URL aUrl = new URL(uri);
+					URLConnection conn = aUrl.openConnection();
+					conn.setUseCaches(true);
+					conn.connect();
+					is = conn.getInputStream();
+
+					
+					BitmapFactory.Options options = new BitmapFactory.Options();
+					options.inJustDecodeBounds = true;
+					BitmapFactory.decodeStream(is, null, options);
+					options.inSampleSize = calculateInSampleSize(options, width, height);
+					options.inJustDecodeBounds = false;
+					mMap = BitmapFactory.decodeStream(is, null, options);
+					if(is!= null){
+						is.close();
+					}
+
 				}
+				if(mMap == null){
+					return null;
+				}
+
+				// save this bitmap into file now
+				fos = new FileOutputStream(f);
+				mMap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+				fos.flush();
+				fos.close();
 				currentCount++;
-				Options opts = new BitmapFactory.Options();
-				opts.inSampleSize = 1;
-				opts.inPurgeable = true;
-				Bitmap aMap =  BitmapFactory.decodeFile(f.getAbsolutePath(), opts);
-				mChache.putBitmapIntoMemCache(uri, aMap);
-				return aMap;
+				mChache.putBitmapIntoMemCache(uri, mMap);
+
+				return mMap;
 			}catch(Exception e){
-				
+				e.printStackTrace();
 			}finally{
 				if(fos != null){
 					try {
 						fos.flush();
 						fos.close();
-						is.close();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-					
+
 				}
 			}
 		}
@@ -149,5 +196,26 @@ public class FileCache {
 	 */
 	public void clearMemoryCache(){
 		mChache.clear();
+	}
+
+
+	// to calculate sample size, of large bitmaps
+	public int calculateInSampleSize(
+			BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			final int halfHeight = height / 2;
+			final int halfWidth = width / 2;
+			while ((halfHeight / inSampleSize) > reqHeight
+					&& (halfWidth / inSampleSize) > reqWidth) {
+				inSampleSize *= 2;
+			}
+		}
+
+		return inSampleSize;
 	}
 }
